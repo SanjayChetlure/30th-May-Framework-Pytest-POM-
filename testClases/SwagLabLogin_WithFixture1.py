4acfe8ebd0c5e5e73594c58478d531766ddbd4a3
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-09-15 21:22:47 +0530

    15th Sept 2026

--- testClases/SwagLabLogin_WithFixture1.py
+++ testClases/SwagLabLogin_WithFixture1.py
@@ -8,7 +8,7 @@ from pageClasses import login,home
 
 class Test_SwagLabLogin:           #MainClass
 
-    def test_TC1_LoginToApp_withValidDetails(self,setup):        #test case / test method
+    def test_TC1_LoginToApp_withValidDetails(self,setup,request):        #test case / test method
         driver=setup
 
         loginObj=login.SwagLabLoginPage(driver)
@@ -26,13 +26,14 @@ class Test_SwagLabLogin:           #MainClass
         if actLogoText==expLogoText:
             assert True
         else:
-            UtilityClass.captureSS(driver,"test_TC1_LoginToApp_withValidDetails")
+            # UtilityClass.captureSS(driver,"test_TC1_LoginToApp_withValidDetails")
+            UtilityClass.captureSS(driver, request.node.name)  # request.node.name -> get name of currently running script
             assert False
 
         time.sleep(2)
         driver.quit()
 
-    def atest_TC2_LoginToApp_withInValidDetails(self,setup):  # test case / test method
+    def atest_TC2_LoginToApp_withInValidDetails(self,setup,request):  # test case / test method
         driver=setup
         loginObj = login.SwagLabLoginPage(driver)
         loginObj.enterUN("abc")
@@ -48,6 +49,8 @@ class Test_SwagLabLogin:           #MainClass
         if actErrorMsg==expErrorMsg:
             assert True
         else:
+            # UtilityClass.captureSS(driver, "test_TC2_LoginToApp_withInValidDetails")
+            UtilityClass.captureSS(driver, request.node.name)
             assert False
 
         time.sleep(2)
